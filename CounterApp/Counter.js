62417cb02e85b486b842fd34b836b3acd8646004
chowtala meghana <meghanachowtala@gmail.com>
2025-06-07 21:54:58 +0530

    counterApp

---
+++ CounterApp/Counter.js
@@ -0,0 +1,9 @@
+let count=0;
+    function increment(){
+        count++;
+        document.getElementById('count').innerText=count;
+    }
+    function decrement(){
+        count--;
+        document.getElementById('count').innerText=count;
+    }
